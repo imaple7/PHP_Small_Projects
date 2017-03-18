77974d7d4ce386c51421ce8cf806897860b67019
Fujie Chen <imaple7@gmail.com>
2017-03-18 14:43:37 -0500

    Try different types of PHP labels and comment functionality.

--- PHPStartUp/Hello.php
+++ PHPStartUp/Hello.php
@@ -4,5 +4,15 @@
  </head>
  <body>
     <?php echo '<p>Hello World Fujie Chen</p>'; ?>
+
+    <p>Show by browser</p>
+    <?php
+        //Comments
+        echo '<p>GOGOGO</p>'
+    ?>
+
+    <script language='php'>echo '<p>Using script tag</p>' </script>
+
+
  </body>
 </html>
